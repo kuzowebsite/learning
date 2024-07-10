c3661def51cf3ff1e827ac1c5b49d01afd0bc4ee
Batuka2003 <m.batireedui@ider.edu.mn>
2024-07-10 15:31:48 +0800

    learning

---
+++ course.php
@@ -0,0 +1,42 @@
+<?php include("header.php"); ?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+
+			<div class="container clearfix">
+				<h1>Зарим курсууд байдаг <strong>Тун удахгүй...!</strong></h1>
+				<span>Онцгой курсууд</span>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Page Sub Menu
+		============================================= -->
+		<div id="page-menu">
+
+			<div id="page-menu-wrap">
+
+
+			</div>
+
+		</div><!-- #page-menu end -->
+
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+					<div class=" bottommargin clearfix ">
+
+						<div class="row">
+							// Watch Code from Video
+						  
+					</div>
+				</div>		
+			</div>
+		</div>
+	</section>
+
+<?php include("footer.php"); ?>
